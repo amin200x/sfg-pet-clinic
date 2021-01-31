88fc24db6aa0099f68f7bac0061f607abbbfb9f9
Amin Ghadimian <amin@gmail.com>
2021-01-31 15:46:53 +0330

    Add Maven Release. Closes #3.

---
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTest.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(classes = {})
+class SfgPetClinicApplicationTest {
+
+    @Test
+    void main() {
+    }
+}
