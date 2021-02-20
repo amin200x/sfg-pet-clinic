5dd0b9cac4e18cbe3bd7d5f948e1e7abaa7db152
Amin Ghadimian <amin@gmail.com>
2021-02-20 11:42:13 +0330

    Added display owner.  Closes #53.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -3,7 +3,9 @@ package guru.springframework.sfgpetclinic.controllers;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping("/owners")
 @Controller
@@ -19,10 +21,29 @@ public class OwnerController {
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
+
     @RequestMapping({"/find"})
     public String findOwners() {
         return "notimplemented";
     }
 
+    @RequestMapping({"/{id}"})
+    public ModelAndView showOwner(@PathVariable String id) {
+        ModelAndView modelAndView = new ModelAndView("/owners/ownerdetail");
+        modelAndView.addObject("owner", ownerService.findById(Long.valueOf(id)));
+        return modelAndView;
+    }
 
+    @RequestMapping({"/{id}/delete"})
+    public String deleteOwner(@PathVariable Long id) {
+        ownerService.deleteById(id);
+        return "redirect:/index";
+    }
+
+
+    @RequestMapping({"/{id}/update"})
+    public String updateOwner(@PathVariable Long id, Model model) {
+       model.addAttribute("owner", ownerService.findById(id));
+        return "owners/ownerform";
+    }
 }
