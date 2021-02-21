f615f14d1a7813ad9937af8cfba7eb5db6c220ae
Amin Ghadimian <amin@gmail.com>
2021-02-21 13:03:33 +0330

    Added  save, update and delete pet.  Closes #56.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/springdatajpa/PetSDJpaService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/springdatajpa/PetSDJpaService.java
@@ -1,22 +1,29 @@
 package guru.springframework.sfgpetclinic.services.map.springdatajpa;
 
 import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.repositories.OwnerRepository;
 import guru.springframework.sfgpetclinic.repositories.PetRepository;
 import guru.springframework.sfgpetclinic.services.PetService;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
+import javax.transaction.Transactional;
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 @Service
 @Profile("springdatajpa")
 public class PetSDJpaService implements PetService {
     private final PetRepository petRepository;
+    private final OwnerRepository ownerRepository;
+    private final PetTypeService petTypeService;
 
-    public PetSDJpaService(PetRepository petRepository) {
+    public PetSDJpaService(PetRepository petRepository, OwnerRepository ownerRepository, PetTypeService petTypeService) {
         this.petRepository = petRepository;
+        this.ownerRepository = ownerRepository;
+        this.petTypeService = petTypeService;
     }
 
     @Override
@@ -31,9 +38,28 @@ public class PetSDJpaService implements PetService {
         return petRepository.findById(id).orElse(null);
     }
 
+    @Transactional
     @Override
     public Pet save(Pet pet) {
-        return petRepository.save(pet);
+        Pet savedPet = new Pet();
+        if (pet.getId() == null) {
+            pet.setOwner(ownerRepository.findById(pet.getOwner().getId()).get());
+            ownerRepository.findById(pet.getOwner().getId()).get().getPets().add(pet);
+            pet.setPetType(petTypeService.save(pet.getPetType()));
+            savedPet = petRepository.save(pet);
+        } else {
+            Optional<Pet> petOptional = petRepository.findById(pet.getId());
+            if (petOptional.isPresent()) {
+                Pet updatePet = petOptional.get();
+                updatePet.getPetType().setName(pet.getPetType().getName());
+                updatePet.setPetType(pet.getPetType());
+                updatePet.setName(pet.getName());
+                updatePet.setBirthDate(pet.getBirthDate());
+                savedPet = petRepository.save(updatePet);
+            }
+        }
+
+        return savedPet;
     }
 
     @Override
---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/PetController.java
@@ -0,0 +1,49 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.model.PetType;
+import guru.springframework.sfgpetclinic.services.PetService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class PetController {
+    private final PetService petService;
+
+    public PetController(PetService petService) {
+        this.petService = petService;
+    }
+
+    @PostMapping("/pets" )
+    public String saveOrUpdatePet(@ModelAttribute Pet pet){
+       Pet savedPet = petService.save(pet);
+      return "redirect:/owners/"+savedPet.getOwner().getId();
+    }
+
+    @GetMapping("/owners/{ownerId}/pets/new")
+    public String saveForm(@PathVariable("ownerId") Long ownerId, Model model){
+        Pet newPet = new Pet();
+        newPet.setOwner(Owner.builder().id(ownerId).build());
+        newPet.setPetType(PetType.builder().build());
+        model.addAttribute("pet", newPet);
+        return "pets/petform";
+    }
+
+    @GetMapping("/pets/{id}/update")
+    public String updateForm(@PathVariable Long id, Model model){
+        model.addAttribute("pet", petService.findById(id));
+        return "pets/petform";
+    }
+
+    @GetMapping("/owners/{ownerId}/pets/{id}/delete")
+    public String updateForm(@PathVariable Long ownerId, @PathVariable Long id){
+
+        petService.deleteById(id);
+        return "redirect:/owners/"+ownerId;
+    }
+}
