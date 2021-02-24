03e6fa94b379a09ac044aa1abd8b6cae0fffa574
Amin Ghadimian <amin@gmail.com>
2021-02-24 15:40:36 +0330

    Added Vets JSON Endpoint. Closes #63

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,9 +1,17 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.VetService;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.awt.*;
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -17,5 +25,9 @@ public class VetController {
     public String listVets(Model model){
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+    @GetMapping(value = {"/apivets"})
+    public  @ResponseBody  Set<Vet> vets(){
+        return vetService.findAll();
     }
 }
